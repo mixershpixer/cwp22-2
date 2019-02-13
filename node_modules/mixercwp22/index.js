860fa03514e819b5a7fc5bf35c6272c7b4740624
mixershpixer <max.grechuha@mail.ru>
2019-02-13 09:01:02 +0300

    last

---
+++ node_modules/mixercwp22/index.js
@@ -0,0 +1,50 @@
+console.log('Welcome to Polish notation. It is v2.0.0');
+
+const Calculator = {
+    calc: (expression) => {
+        let stack = [];
+        let tokens = expression.split(' ');
+
+        for(let i = 0; i < tokens.length; i++){
+            let token = tokens[i];
+
+            if(Calculator.isNumber(token)){
+                stack.push(parseInt(token));
+            }
+            if(Calculator.isOperand(token)){
+                stack.push( 
+                    Calculator.doOperation(
+                        token,
+                        stack.pop(),
+                        stack.pop()
+                    )
+                );
+            }
+        }
+        return stack.pop();
+    },
+
+    doOperation: (operator, x, y) => {
+        return Calculator.operations[operator](y, x); 
+    },
+
+    operations: {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y
+    },
+
+    isNumber: token => {
+        return /\d+/.test(token);
+    },
+
+    isOperand: token => {
+        return /[-\+\*\/]/.test(token);
+    }
+}
+
+module.exports = (expression, SomeText) => {
+    console.log(SomeText);
+    return Calculator.calc(expression);
+};
